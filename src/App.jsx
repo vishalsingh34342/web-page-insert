9c371fb911d2dcdd3fe3b20e05c0adeec3127801
Vishal Singh <jashanchoudhary430@gmail.com>
2026-03-23 12:47:36 +0530

    start hero sec

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Nav from './components/Nav'
+import Hero from './components/Hero'
 
 const App = () => {
   return (
     <>
     <Nav />
+    <Hero />
     
     </>
   )
---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='h-screen w-full bg-gray-600 flex justify-center items-center'>
+        <h1>this is sample task</h1>
+        <h2>it is only for prac</h2>
+
+    </div>
+  )
+}
+
+export default Hero
