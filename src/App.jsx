86ad5ec92a5c6c85309772825ce12490a30f1b18
Vishal Singh <jashanchoudhary430@gmail.com>
2026-03-23 12:09:28 +0530

    Added navbar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from './components/Nav'
+
+const App = () => {
+  return (
+    <>
+    <Nav />
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className='w-full py-10 bg-red-500 flex justify-between px-5'>
+        <h2>name</h2>
+
+        <ul className='flex gap-20'>
+            <li>home</li>
+            <li>home</li>
+            <li>home</li>
+            <li>home</li>
+        </ul>
+
+    </div>
+  )
+}
+
+export default Nav
