bad21965ea82230e2fafdee364b7737d75d99441
Vishal Singh <jashanchoudhary430@gmail.com>
2026-03-23 14:55:25 +0530

    start footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Nav from './components/Nav'
 import Hero from './components/Hero'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <>
     <Nav />
     <Hero />
+    <Footer />
     
     </>
   )
