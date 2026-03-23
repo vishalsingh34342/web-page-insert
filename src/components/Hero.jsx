62a13a75e0c3b483155cc89a5f83d9ab35b1d9cd
Vishal Singh <jashanchoudhary430@gmail.com>
2026-03-23 12:49:32 +0530

    Completed hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,6 +5,7 @@ const Hero = () => {
     <div className='h-screen w-full bg-gray-600 flex justify-center items-center'>
         <h1>this is sample task</h1>
         <h2>it is only for prac</h2>
+        <button className='px-10 py-3 bg-green-600 rounded-2xl'>Click me</button>
 
     </div>
   )
